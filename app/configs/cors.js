a4b82bed91149e8b6fb9782b69e94aae4c7d9c44
pepecachivache <giuzepelin@gmail.com>
2019-12-04 19:00:43 -0300

    test appu

--- app/configs/cors.js
+++ app/configs/cors.js
@@ -1,6 +1,6 @@
 module.exports = {
      corsOptions: corsOptions = {
-        origin: process.env.NODE_ENV === "development" ? 'http://locahost:3000': 'https://ganymede.herokuapp.com/',
+        origin: process.env.NODE_ENV === "development" ? 'http://locahost:3000': 'https://ganymede.herokuapp.com',
         optionsSuccessStatus: 200
       }
 }
